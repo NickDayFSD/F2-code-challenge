9019720d9bd820a7cb094b4d5d3f364ddd2fff3a
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-04-26 23:07:35 -0700

    code challenges complete

--- src/code-challenges/01-challenges/array-map.js
+++ src/code-challenges/01-challenges/array-map.js
@@ -1,24 +1,46 @@
 function doubleNum(x) {
   return x * 2;
 }
+export function doubleNumbers(arr) {
+  return arr.map(doubleNum);
+}
 
 function stringNum(x) {
   return JSON.stringify(x);
+}
+export function stringyNumbers(arr) {
+  return arr.map(stringNum);
 }
 
 function capName(x) {
   const lower = x.toLowerCase();
   return lower.charAt(0).toUpperCase() + lower.slice(1);
 }
-
-export function doubleNumbers(arr) {
-  return arr.map(doubleNum);
-}
-
-export function stringyNumbers(arr) {
-  return arr.map(stringNum);
-}
-
 export function capitalizeNames(arr) {
   return arr.map(capName);
+}
+
+function grabName(object) {
+  return object.name;
+}
+export function namesOnly(arr) {
+  return arr.map(grabName);
+}
+
+function checkMovie(object) {
+  if (object.age < 18) {
+    return `${object.name} is under age!!`;
+  } else {
+    return `${object.name} can go to The Matrix`;
+  }
+}
+export function makeStrings(arr) {
+  return arr.map(checkMovie);
+}
+
+function prepareForDOM(object) {
+  return `<h1>${object.name}</h1><h2>${object.age}</h2>`;
+}
+export function readyToPutInTheDOM(arr) {
+  return arr.map(prepareForDOM);
 }
--- src/code-challenges/01-challenges/array-map.test.js
+++ src/code-challenges/01-challenges/array-map.test.js
@@ -1,4 +1,11 @@
-import { doubleNumbers, stringyNumbers, capitalizeNames } from './array-map.js';
+import {
+  doubleNumbers,
+  stringyNumbers,
+  capitalizeNames,
+  namesOnly,
+  makeStrings,
+  readyToPutInTheDOM
+} from './array-map.js';
 
 test('take an array of numbers and double them', () => {
   const input = [2, 5, 100];
@@ -18,9 +25,107 @@ test('Capitalize names', () => {
   expect(output).toEqual(['John', 'Jacob', 'Jingleheimer', 'Schmidt']);
 });
 
-// test('test description here', () => {
-//   const input = [2, 5, 100];
-//   const output = doubleNumbers(input);
-//   expect(output).toEqual([4, 10, 200]);
-// });
+test('should produce an array of names only', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ];
 
+  const output = namesOnly(input);
+
+  expect(output).toEqual([
+    'Angelina Jolie',
+    'Eric Jones',
+    'Paris Hilton',
+    'Kayne West',
+    'Bob Ziroll'
+  ]);
+});
+
+test('produce a name and if they can go see the Matrix', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ];
+
+  const output = makeStrings(input);
+
+  expect(output).toEqual([
+    'Angelina Jolie can go to The Matrix',
+    'Eric Jones is under age!!',
+    'Paris Hilton is under age!!',
+    'Kayne West is under age!!',
+    'Bob Ziroll can go to The Matrix'
+  ]);
+});
+
+test('create h1 and h2 with user data', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ];
+
+  const output = readyToPutInTheDOM(input);
+
+  expect(output).toEqual([
+    '<h1>Angelina Jolie</h1><h2>80</h2>',
+    '<h1>Eric Jones</h1><h2>2</h2>',
+    '<h1>Paris Hilton</h1><h2>5</h2>',
+    '<h1>Kayne West</h1><h2>16</h2>',
+    '<h1>Bob Ziroll</h1><h2>100</h2>'
+  ]);
+});
