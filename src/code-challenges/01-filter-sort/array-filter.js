3a4c4bcd791d2c1aa8ecfaaac81ca2ab0ba7aa38
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-04-27 19:07:38 -0700

    five characters test passed

--- src/code-challenges/01-filter-sort/array-filter.js
+++ src/code-challenges/01-filter-sort/array-filter.js
@@ -1,3 +1,7 @@
 export function evensOnly(arr) {
   return arr.filter(num => num % 2 === 0);
 }
+
+export function fiveCharactersOrFewerOnly(arr) {
+  return arr.filter(word => word.length < 6);
+}
--- src/code-challenges/01-filter-sort/array-filter.test.js
+++ src/code-challenges/01-filter-sort/array-filter.test.js
@@ -1,9 +1,16 @@
 import {
-  evensOnly
+  evensOnly,
+  fiveCharactersOrFewerOnly
 } from './array-filter';
 
-test('Five and greater only', () => {
+test('Even numbers only', () => {
   const input = [3, 6, 8, 2];
   const output = evensOnly(input);
   expect(output).toEqual([6, 8, 2]);
+});
+
+test('five characters or fewer', () => {
+  const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping'];
+  const output = fiveCharactersOrFewerOnly(input);
+  expect(output).toEqual(expect.arrayContaining(['by', 'dog', 'wolf', 'eaten']));
 });
