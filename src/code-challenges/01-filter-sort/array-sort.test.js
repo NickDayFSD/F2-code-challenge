e770e9295e0586f16807b07525f85674e5a75534
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-04-27 19:32:52 -0700

    greatest to least done

--- src/code-challenges/01-filter-sort/array-sort.test.js
+++ src/code-challenges/01-filter-sort/array-sort.test.js
@@ -1,9 +1,17 @@
 import {
-  leastToGreatest
+  leastToGreatest,
+  greatestToLeast
 } from './array-sort.js';
 
-test('test notes here', () => {
+test('sort lowest to greatest', () => {
   const input = [1, 3, 5, 2, 90, 20];
   const output = leastToGreatest(input);
   expect(output).toEqual([1, 2, 3, 5, 20, 90]);
 });
+
+test('sort greatest to lowest', () => {
+  const input = [1, 3, 5, 2, 90, 20];
+  const output = greatestToLeast(input);
+  expect(output).toEqual([90, 20, 5, 3, 2, 1]);
+});
+
