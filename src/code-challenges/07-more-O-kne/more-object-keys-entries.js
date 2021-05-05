0300fdcf52334d62310a37c070fe7d032693a9a9
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-04 22:43:32 -0700

    whatever I could do I guess

--- src/code-challenges/07-more-O-kne/more-object-keys-entries.js
+++ src/code-challenges/07-more-O-kne/more-object-keys-entries.js
@@ -6,7 +6,26 @@ export function getHouses(arr) {
 
 export function makeArray(obj) { // updateNumbers is a stupid name
   const array = [];
-  Object.entries(obj).forEach((n, index) => array.push(n[0] + ': ' + n[1]));
+  Object.entries(obj).forEach(n => array.push(n[0] + ': ' + n[1]));
   return array;
 }
 
+export function totalCharacters(arr) {
+  let charCount = [];
+  let trueCount = 0;
+  // loop through array
+  for (let obj of arr) {
+    delete obj.house;
+    trueCount += obj.children.length;
+    delete obj.children;
+    const values = Object.values(obj);
+    for (let name of values) {
+      if (name !== null) {
+        charCount.push(name);
+      }
+    }
+    trueCount += charCount.length;
+  }
+  return trueCount;
+  // count all values
+}
--- src/code-challenges/07-more-O-kne/more-object-keys-entries.test.js
+++ src/code-challenges/07-more-O-kne/more-object-keys-entries.test.js
@@ -1,6 +1,7 @@
 import {
   getHouses,
-  makeArray
+  makeArray,
+  totalCharacters
 } from './more-object-keys-entries.js';
 
 test('it gets the names of the houses', () => {
@@ -56,4 +57,53 @@ test('turns an object into an array', () => { // updateNumbers is a stupid name
   const input = { 'Grace Hopper': '222-303-5938', 'Ada Lovelace': '222-349-9842', 'Alan Turing': '222-853-5933' };
   const output = makeArray(input);
   expect(output).toEqual(['Grace Hopper: 222-303-5938', 'Ada Lovelace: 222-349-9842', 'Alan Turing: 222-853-5933']);
+});
+
+test('this is a headcount', () => {
+  const characters = [
+    {
+      name: 'Eddard',
+      spouse: 'Catelyn',
+      children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+      house: 'Stark' // 7
+    },
+    {
+      name: 'Jon A.',
+      spouse: 'Lysa',
+      children: ['Robin'],
+      house: 'Arryn' // 3
+    },
+    {
+      name: 'Cersei',
+      spouse: 'Robert',
+      children: ['Joffrey', 'Myrcella', 'Tommen'],
+      house: 'Lannister' // 5
+    },
+    {
+      name: 'Daenarys',
+      spouse: 'Khal Drogo',
+      children: ['Drogon', 'Rhaegal', 'Viserion'],
+      house: 'Targaryen' // 5
+    },
+    {
+      name: 'Mace',
+      spouse: 'Alerie',
+      children: ['Margaery', 'Loras'],
+      house: 'Tyrell' // 4
+    },
+    {
+      name: 'Euron',
+      spouse: null,
+      children: [],
+      house: 'Greyjoy' //1
+    },
+    {
+      name: 'Jon S.',
+      spouse: null,
+      children: [],
+      house: 'Snow' //1
+    }
+  ]; //arrange
+  const output = totalCharacters(characters); //act
+  expect(output).toBe(26); //assert
 });
