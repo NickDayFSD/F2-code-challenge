d9526ea6580f8a5e7f8b8b54808e9a1f8ef065dc
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-07 15:20:36 -0700

    first test passes

---
+++ src/code-challenges/08-array-methods/array-methods.js
@@ -0,0 +1,38 @@
+export function howMuchPencil(str) {
+  const strArray = [str];
+  let sharp = str;
+  for (let i = 0; i < str.length; i++) {
+    sharp = sharp.slice(1);
+    strArray.push(sharp);
+  }
+  return strArray;
+}
+
+export function wordsToCharList(arr) {
+
+}
+
+export function listFoods(recipe) {
+
+}
+
+export function stepActions(recipe) {
+
+}
+
+export function removeLastCharacters(str, numberOfCharacters) {
+
+}
+
+// stretch goals 
+export function totalSumCSV(str) {
+
+}
+
+export function removeVowels(str) {
+
+}
+
+export function extractVowels(str) {
+
+}
