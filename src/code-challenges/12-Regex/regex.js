fdfe79cfd20496dafa1a75288615559163d902b2
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-13 20:00:21 -0700

    Let's give this Regex stuff a try

--- src/code-challenges/12-Regex/regex.js
+++ src/code-challenges/12-Regex/regex.js
@@ -1,3 +1,3 @@
-export function sortByChildren(arr) {
+export function sortByChildren(charArray) {
 
 }
--- src/code-challenges/12-Regex/regex.test.js
+++ src/code-challenges/12-Regex/regex.test.js
@@ -47,3 +47,9 @@ let characters = [
     house: 'Snow'
   }
 ];
+
+
+test('It should sort the characters by number of children', () => {
+  expect(sortByChildren(characters)[0].name).toStrictEqual('Euron');
+  expect(sortByChildren(characters)[0].children.length).toStrictEqual(0);
+});
